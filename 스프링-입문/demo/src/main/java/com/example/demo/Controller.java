72a6d3c786b94ee79ec04a67beab8b6399ad9b7b
NyeongC <cjfsud23@naver.com>
2023-10-24 21:40:34 +0900

    API 테스트

--- 스프링-입문/demo/src/main/java/com/example/demo/Controller.java
+++ 스프링-입문/demo/src/main/java/com/example/demo/Controller.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -10,5 +13,31 @@ public class Controller {
     public String hello(Model model){
         model.addAttribute("data","hello!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello h1 = new Hello();
+        h1.setName(name);
+        return h1;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
