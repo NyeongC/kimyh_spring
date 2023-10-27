277707f69c2afc09aa857881640b92f50ac525b2
NyeongC <cjfsud23@naver.com>
2023-10-27 19:25:08 +0900

    Add MemberService

---
+++ 스프링-입문/demo/src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+
+    public Long join(Member member){
+
+        // 유효성 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원 입니다.");
+                });
+    }
+
+    /*
+    * 회원 전체 조회
+    * */
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
