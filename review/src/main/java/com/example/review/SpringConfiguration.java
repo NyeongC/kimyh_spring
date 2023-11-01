8b531412e1b4b680719ebac3556750e221f501db
NyeongC <cjfsud23@naver.com>
2023-11-01 22:25:02 +0900

    DB연결 jdbcTemplate

--- review/src/main/java/com/example/review/SpringConfiguration.java
+++ review/src/main/java/com/example/review/SpringConfiguration.java
@@ -1,13 +1,21 @@
 package com.example.review;
 
+import com.example.review.repository.JdbcTemplateMemberRepository;
 import com.example.review.repository.MemberRepository;
 import com.example.review.repository.MemoryMemberRepository;
 import com.example.review.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfiguration {
+
+    private final DataSource dataSource;
+    public SpringConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +24,8 @@ public class SpringConfiguration {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
